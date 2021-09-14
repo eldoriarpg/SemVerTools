642ceef7cdb1bc2cd388f6327f81da9e10b1aa52
SirYwell <hannesgreule@outlook.de>
2021-09-14 10:32:54 +0200

    Add convenience methods

--- src/main/java/de/eldoria/semvertools/SemanticVersion.java
+++ src/main/java/de/eldoria/semvertools/SemanticVersion.java
@@ -37,11 +37,45 @@ public interface SemanticVersion extends Comparable<SemanticVersion> {
 
   SemanticVersion withMajor(int major);
 
+  default SemanticVersion increaseMajor() {
+    return this.withMajor(this.major() + 1);
+  }
+
   SemanticVersion withMinor(int minor);
 
+  default SemanticVersion increaseMinor() {
+    return this.withMinor(this.minor() + 1);
+  }
+
   SemanticVersion withPatch(int patch);
+
+  default SemanticVersion increasePatch() {
+    return this.withPatch(this.patch() + 1);
+  }
 
   SemanticVersion withPreRelease(@Nullable PreRelease preRelease);
 
   SemanticVersion withBuild(@Nullable Build build);
+
+  /**
+   * Returns whether this version precedes ("is older than") the other version.
+   * This is equivalent to checking {@code this.compareTo(other) < 0}.
+   *
+   * @param other the version to compare with.
+   * @return {@code true} if this version is a predecessor of the other version.
+   */
+  default boolean precedes(SemanticVersion other) {
+    return compareTo(other) < 0;
+  }
+
+  /**
+   * Returns whether this version is succeeds ("is newer than") the other version.
+   * This is equivalent to checking {@code this.compareTo(other) > 0}.
+   *
+   * @param other the version to compare with.
+   * @return {@code true} if this version is a successor of the other version.
+   */
+  default boolean succeeds(SemanticVersion other) {
+    return compareTo(other) > 0;
+  }
 }

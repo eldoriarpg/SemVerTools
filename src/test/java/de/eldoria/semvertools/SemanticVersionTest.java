29ae2d37374a0281d9e9bb2e184d0328c9130313
Ilja Korneckis <ilja.korneckis@gmail.com>
2021-10-05 21:01:05 +0200

    feat: add module descriptor (#10)
    * feat: add module descriptor
    
    Also reduces visibility of VersionCore, so only the interface
    is exposed.
    
    * feat: expose VersionParseException
    
    The exception is part of the interface, so should be exposed by the
     module. This will also allow clients to catch it, if they want.

--- src/test/java/de/eldoria/semvertools/SemanticVersionTest.java
+++ src/test/java/de/eldoria/semvertools/SemanticVersionTest.java
@@ -6,13 +6,12 @@
 
 package de.eldoria.semvertools;
 
-import de.eldoria.semvertools.parser.VersionParseException;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class SemanticVersionTest {
+class SemanticVersionTest {
   @ParameterizedTest
   @ValueSource(strings = {
       // don't allow totally random strings

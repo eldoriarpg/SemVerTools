2c70b9a3c5fb53bd3a58c0ab1241a2d9432d333f
SirYwell <hannesgreule@outlook.de>
2022-02-05 16:31:31 +0100

    license header

--- src/main/java/de/eldoria/semvertools/package-info.java
+++ src/main/java/de/eldoria/semvertools/package-info.java
@@ -1,3 +1,9 @@
+/*
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright (c) 2021 SemVerTools team and contributors
+ */
+
 /**
  * This is the main package of the SemVerTools library. It contains the classes needed to construct,
  * compare and inspect semantic versions.
